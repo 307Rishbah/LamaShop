7effb162968ea96ae0565e113083ec70204ca563
Rishabh Vishwakarma <rishabhvishwakarma307@gmail.com>
2023-05-16 01:55:04 +0530

    third commit

--- Server/routers/product.js
+++ Server/routers/product.js
@@ -1,5 +1,5 @@
 const Product = require("../models/Product");
-const { Authorization, IsAdmin } = require("./Verifytoken");
+const { IsAdmin } = require("./VerifyToken");
 
 const router = require("express").Router();
 
