7995671d17ebd2d161938f59a3bf289807160363
Rishabh Vishwakarma <rishabhvishwakarma307@gmail.com>
2023-05-16 02:04:17 +0530

    verifiyToken.js renamed

--- Server/routers/product.js
+++ Server/routers/product.js
@@ -1,5 +1,5 @@
 const Product = require("../models/Product");
-const { IsAdmin } = require("./VerifyToken");
+const { IsAdmin } = require("./verifyToken");
 
 const router = require("express").Router();
 
