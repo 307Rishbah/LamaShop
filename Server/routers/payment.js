279d2926fd489106a8d7eda563112f18e7f0799d
Rishabh Vishwakarma <rishabhvishwakarma307@gmail.com>
2023-06-13 19:21:56 +0530

    payment testing 10

--- Server/routers/payment.js
+++ Server/routers/payment.js
@@ -17,9 +17,9 @@ router.post("/payment", async (req, res) => {
       receipt: "receipt_order_74394",
     };
 
-    // const order = await instance.orders.create(options);
+    const order = await instance.orders.create(options);
 
-    // if (!order) return res.status(500).send("Some error occured");
+    if (!order) return res.status(500).send("Some error occured");
 
     res.json("order");
   } catch (error) {
